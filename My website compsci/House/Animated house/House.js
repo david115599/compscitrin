4abc90e8c7ec0f03c9d6b912e516998c5046163d
david115599 <davidbershadsky@RaspberryPi-2.home>
2017-09-17 21:28:53 -0400

    k
    K

--- My website compsci/House/Animated house/House.js
+++ My website compsci/House/Animated house/House.js
@@ -7,10 +7,17 @@ function setup() {
   createCanvas(W,H);
   background(100,255,100);
 
-  fill(10,80,255);
+  fill(10,80,200);
   rect(0, 0, W, H/1.5);
-  fill(0,0,255);
+  fill(0,0,180);
   rect(W/2-W/4/2,H/2.5,W/4,H/2);
   triangle(W/3,H/2.5,W/2, H/5, W/1.5, H/2.5);
+  fill(150,100,100);
+  rect(700,570,75,150);
+  fill(255,255,255);
+  rect(580,540,75,75);
+  rect(800,450,75,75);
+  rect(580,360,75,75);
+
 
 }
