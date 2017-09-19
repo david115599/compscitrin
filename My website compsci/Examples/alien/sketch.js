745f4297671409750226d58cbe8df59bfe44a93b
david115599 <davidbershadsky@RaspberryPi-2.home>
2017-09-18 21:31:21 -0400

    kljn
    Nmjkl

--- My website compsci/Examples/alien/sketch.js
+++ My website compsci/Examples/alien/sketch.js
@@ -1,6 +1,8 @@
 
 var y = 0;
-var v = 10
+var x = 0;
+var v = 10;
+var q = 10;
 //var bg = loadImage("House.png");
 
 function setup() {
@@ -17,15 +19,15 @@ function draw() {
   line(0, y, width, y);
   background(255)
   fill(0);
-  ellipse(100, y+100, 80, 80);
-  ellipse(150, y+150, 80, 80);
-  ellipse(100, y+50, 80, 80);
-  ellipse(200, y+100, 80, 80);
-  ellipse(250, y+150, 80, 80);
+  ellipse(x+100, y+100, 80, 80);
+  ellipse(x+150, y+150, 80, 80);
+  ellipse(x+100, y+50, 80, 80);
+  ellipse(x+200, y+100, 80, 80);
+  ellipse(x+250, y+150, 80, 80);
   fill(255,0,0);
-  ellipse(120, y+35, 5, 5);
-  ellipse(80, y+35, 5, 5);
-  ellipse(100, y+50, 20, 40);
+  ellipse(x+120, y+35, 5, 5);
+  ellipse(x+80, y+35, 5, 5);
+  ellipse(x+100, y+50, 20, 40);
 
 
 if (y > -1 & y < height-200) {
@@ -34,6 +36,13 @@ if (y > -1 & y < height-200) {
 else{
     v = -v
     y = y+v+++v^2;
+  }
+  if (x > -1 & x < width-200) {
+    x = x+q+++q^2;
+  }
+else{
+    q = -q
+    x = x+q+++q^2;
   }
 }
 
