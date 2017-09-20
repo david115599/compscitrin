1c475bdd2e45dbe35be292c4d11d90a11e06d5f1
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-09-20 09:14:44 -0400

    Sadas
    And

--- My website compsci/Examples/Lab/sketch.js
+++ My website compsci/Examples/Lab/sketch.js
@@ -2,7 +2,7 @@
 var y = 0;
 var x = 0;
 var v = 1;
-var q = 2;
+var q = 4;
 var sgy = 0;
 var sgx = 0;
 var sgv = 1;
@@ -15,6 +15,10 @@ var sry = 0;
 var srx = 0;
 var srv = 2;
 var srq = 2;
+var spy = 0;
+var spx = 0;
+var spv = 2;
+var spq = 2;
 function setup() {
 
 
@@ -24,19 +28,19 @@ function setup() {
 function draw() {
 //Moving Ball
 stroke(0, 0, 0);
-background(255)
+background(200)
 fill(255,255,0);
 ellipse(x+100, y+100, 20, 20);
 
 
-if (y > -1 & y < height-200) {
+if (y > -1 & y < height-100) {
   y = y+v;
 }
 else{
   v = -v
   y = y+v;
 }
-if (x > -1 & x < width-200) {
+if (x > -1 & x < width-100) {
   x = x+q;
 }
 else{
@@ -46,7 +50,7 @@ else{
   //Grean square
   stroke(0, 0, 0);
   fill(0,255,0);
-  rect(sgx+200, sgy+100, 50, 50);
+  rect(sgx+200, sgy+0, 50, 50);
 
 
   if (sgy > -1 & sgy < height-150) {
@@ -82,6 +86,19 @@ else{
     srv = -srv
     sry = sry+srv;
   }
+  //Pulsing Ball
+stroke(0, 0, 0);
+fill(0,0,0);
+ellipse(700, 350, spx+5, spx+5);
+
+
+if (spx > -1 & spx < 150) {
+  spx = spx+spv;
+}
+else{
+  spv = -spv
+  spx = spx+spv;
+}
 
 }
 
