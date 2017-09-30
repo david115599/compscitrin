b990d9aa72fa6fdada042bdd2e74973a49838fa8
david115599 <davidbershadsky@new-host-4.home>
2017-09-29 20:56:18 -0400

    Ads
    Adsasd

--- My website compsci/House/Animated house/House.js
+++ My website compsci/House/Animated house/House.js
@@ -5,22 +5,18 @@ var x=60;
 var starx = [];
 var stary = [];
 var starw = [];
-var sx = 0;
-var sy = 0;
-var sxv = 2;
-var syv = 2;
-var beginX = 20.0;  // Initial x-coordinate
-var beginY = 10.0;  // Initial y-coordinate
-var endX = 570.0;   // Final x-coordinate
-var endY = 320.0;   // Final y-coordinate
+var beginX = -700.0;  // Initial x-coordinate
+var beginY = 800.0;  // Initial y-coordinate
+var endX = 90.0;   // Final x-coordinate
+var endY = -20.0;   // Final y-coordinate
 var distX;          // X-axis distance to move
 var distY;          // Y-axis distance to move
-var exponent = 4;   // Determines the curve
+var exponent = 1/4;   // Determines the curve
 var xs = 0.0;        // Current x-coordinate
 var ys = 0.0;        // Current y-coordinate
-var step = 0.01;    // Size of each step avar the path
-var pct = 0.0;   
-
+var step = 0.005;    // Size of each step avar the path
+var pct = 0.0;   // Percentage traveled (0.0 to 1.0)
+var pct2 = 0.0;      // Percentage traveled (0.0 to 1.0)
 
 function setup() {
 
@@ -39,35 +35,40 @@ function draw() {
 
 //sun/moon
 fill(0, 2);
-  rect(0, 0, width, height);
-  pct += step;
-  if (pct < 1.0) {
-    xs = beginX + (pct * distX);
-    ys = beginY + (pow(pct, exponent) * distY);
-  }
-  fill(255);
-  ellipse(xs, ys, 20, 20);
-if (sx > -1 & sx < width) {
-    sx = sx+sxv;
-  }
-  else{
-    sxv = -sxv
-    sx = sx+sxv;
-  }
-  if (sy > -1 & sy < 400) {
-    sy = sy+syv;
-  }
-  else{
-    syv = -syv
-    sy = sy+syv;
-  }
-  fill(255,255,0);
-ellipse(sx,sy,80,80)
+rect(0, 0, width, height);
+pct += step;
+;
+if (pct < 1.0) {
+  xs = beginX + (pct * distX);
+  ys = beginY + (pow(pct, exponent) * distY);
+}
+else {
+
+beginX = 20.0;  // Initial x-coordinate
+beginY = 10.0;  // Initial y-coordinate
+endX = 570.0;   // Final x-coordinate
+endY = 320.0;   // Final y-coordinate
+distX;          // X-axis distance to move
+distY;          // Y-axis distance to move
+exponent = 4;   // Determines the curve
+xs = 0.0;        // Current x-coordinate
+ys = 0.0;        // Current y-coordinate
+step = 0.01;    // Size of each step along the path
+pct2 += step
+distX = endX - beginX;
+distY = endY - beginY;
+xs = beginX + (pct2 * distX);
+ys = beginY + (pow(pct2, exponent) * distY);
+}
+fill(255);
+ellipse(xs+width/2, ys, 20, 20);
+
+
 //stars
 for(var i = 0; i<50; i++){
-    fill(255,0,0);
-    ellipse(starx[i],stary[i],starw[i],starw[i]);
-  }
+  fill(255,0,0);
+  ellipse(starx[i],stary[i],starw[i],starw[i]);
+}
 //House
 fill(100,255,100);
 rect(0,500, W, H/2);
