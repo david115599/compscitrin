ca8732335e7a5653879fb338586f24ec7b961ec5
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-10-12 09:19:47 -0400

    S
    S

--- My website compsci/Examples/Line Thing/Test.js
+++ My website compsci/Examples/Line Thing/Test.js
@@ -1,5 +1,5 @@
 var W=(800);
-var H=(600);
+var H=(500);
 
 
 function setup() {
@@ -7,12 +7,12 @@ function setup() {
   createCanvas(W,H);
   background(100);
   background(0);
-  for(var i = 10; i < W/2; i += 7) {
+  for(var i = 80; i < W/2; i += 7) {
      stroke(0,255,0);
      line(W/5, i-50, 700,i-50 );
      stroke(255,0,0);
-     line(100, i-49, 600,i-49 );
+     line(70, i-49, 600,i-49 );
      stroke(0,0,255);
-     line(125, i+149, 650,i+149 );
+     line(115, i+46, 655,i+46 );
    }
 }
