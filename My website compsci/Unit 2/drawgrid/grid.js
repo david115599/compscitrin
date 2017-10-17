aebd409c2b47c40ab108d893e8649c23248c4f72
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-10-17 15:29:57 -0400

    S
    S

---
+++ My website compsci/Unit 2/drawgrid/grid.js
@@ -0,0 +1,30 @@
+W=(401)
+H=(401)
+
+
+function setup() {
+
+  createCanvas(W,H);
+  background(255,255,255);
+}
+
+function draw(){
+Grid();
+}
+function mouseClicked() {
+  ellipse(mouseX, mouseY, 40, 40);
+  // prevent default
+  return true;
+}
+function Grid(){
+  for(var i = .5; i < W; i += 10) {
+     strokeWeight(1);
+     stroke(0,255,0);
+     line(W, i,0,i );
+     line(i, 0,i,W );
+     stroke(255,0,0);
+     strokeWeight(3);
+     line(W/2, 0,W/2,W );
+     line(H,H/2,0,H/2 );
+   }
+}
