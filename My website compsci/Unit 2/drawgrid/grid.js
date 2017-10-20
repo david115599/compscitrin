a296c5d0afd85d784e6e08c260cea6c0d007ffb0
nsmadoff <nsmadoff@gmail.com>
2017-10-20 13:13:01 -0400

    d
    d

--- My website compsci/Unit 2/drawgrid/grid.js
+++ My website compsci/Unit 2/drawgrid/grid.js
@@ -1,28 +1,19 @@
-var W=(401);
-var H=(401);
-
-function setup() {
-
-  createCanvas(W,H);
-  background(255,255,255);
-
-  rSlider = createSlider(0, 50, 0, .009);
-  rSlider.position(20, 20);
-  var r=rSlider;
+function setup(){
+createCanvas (400,400);
+drawGrid(25);
 }
 
 function draw(){
-Grid();
-ellipse(0, 0, r, r);
+
 }
 
-function Grid(){
-  for(var i = .5; i < W; i += 20) {
-     strokeWeight(1);
-     line(W, i,0,i );
-     line(i, 0,i,W );
-     strokeWeight(3);
-     line(W/2, 0,W/2,W );
-     line(H,H/2,0,H/2 );
-   }
+function drawGrid(size){
+for (var i = 0; i < width; i += size){
+  line(0,i,width,i);
+  line(i,0,i,height)
+  }
+  strokeWeight(3);
+  line(width/2,0,width/2,height);
+  line(0,height/2,width,height/2);
+  strokeWeight(1);
 }
