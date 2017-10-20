d8af2746f63d50147f079cf2302b4e3ccbe39fdc
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-10-20 19:33:18 -0400

    S
    s

--- My website compsci/Unit 2/Wasteland for testing/wasteland.js
+++ My website compsci/Unit 2/Wasteland for testing/wasteland.js
@@ -5,20 +5,80 @@ var a = (0);
 var b = (0);
 var slider;
 var val;
+var boxSize = 75;
+var overBox = false;
+var locked = false;
+var overBox2 = false;
+var locked2 = false;
+var bx;
+var by;
 function setup(){
-createCanvas (400,400);
- slider = createSlider(0, 255, 100);
+createCanvas (1000,1000);
+ /*slider = createSlider(0, 255, 100);
  slider.position(10, 10);
- slider.style('width', '80px');
+ slider.style('width', '80px');*/
 }
 
 function draw(){
+background(150,150,150);
 drawGrid(25);
 ellipse (x-a, y-b, r, r);
-val = slider.value();
-r == val;
-background(val);
+/*val = slider.value();
+r == val;*/
+// Test if the cursor is over the box
+fill(0,255,0);
+rect(950,0,50,50);
+fill(255,0,0);
+rect(950,50,50,50);
+  if (mouseX > 950 && mouseX < 1000 &&
+      mouseY > 0 && mouseY < 50) {
+    overBox = true;
+    if(!locked) {
+      stroke(255);
+      fill(153);
+      r=r+1;
+    }
+  } else {
+    stroke(0);
+    fill(153);
+    overBox = false;
+  }
+  if (mouseX > 950 && mouseX < 1000 &&
+      mouseY > 50 && mouseY < 100) {
+    overBox2 = true;
+    if(!locked2) {
+      stroke(255);
+      fill(153);
+      r=r-1;
+    }
+  } else {
+    stroke(0);
+    fill(153);
+    overBox2 = false;
+  }
+
 }
+
+
+// temporary slider replacement
+function mousePressed() {
+  if(overBox) {
+    locked = true;
+    console.log("trueup")
+  } else {
+    locked = false;
+  }
+  if(overBox2) {
+    locked2 = true;
+    console.log("truedown")
+  } else {
+    locked2 = false;
+  }
+  xOffset = mouseX-bx;
+  yOffset = mouseY-by;
+}
+
+
 function drawGrid(size){
 for (var i = 0; i < width; i += size){
   line(0,i,width,i);
