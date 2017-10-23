6cdd2db8a8a518ddcaf6b26e559a020155ec1f6e
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-10-23 09:24:26 -0400

    S
    s

--- My website compsci/Unit 2/Wasteland for testing/wasteland.js
+++ My website compsci/Unit 2/Wasteland for testing/wasteland.js
@@ -23,57 +23,9 @@ background(150,150,150);
 drawGrid(25);
 ellipse ((width/2)-a, (height/2)-b, r, r);
 val = slider.value();
-r == val;
-// Test if the cursor is over the box
-fill(0,255,0);
-rect(width-50,0,50,50);
-fill(255,0,0);
-rect(width-50,50,50,50);
-  if (mouseX > width-50 && mouseX < width &&
-      mouseY > 0 && mouseY < 50) {
-    overBox = true;
-    if(!locked) {
-      stroke(0,255,0);
-      fill(153);
-      r=r+1;
-    }
-  } else {
-    stroke(0);
-    fill(153);
-    overBox = false;
-  }
-  if (mouseX > width-50 && mouseX < width &&
-      mouseY > 50 && mouseY < 100) {
-    overBox2 = true;
-    if(!locked2) {
-      stroke(255,0,0);
-      fill(153);
-      r=r-1;
-    }
-  } else {
-    stroke(0);
-    fill(153);
-    overBox2 = false;
-  }
-
+r = val;
 }
 
-
-// temporary slider replacement
-function mousePressed() {
-  if(overBox) {
-    console.log("trueup")
-  } else {
-    locked = false;
-  }
-  if(overBox2) {
-    console.log("truedown")
-  } else {
-    locked2 = false;
-  }
-  xOffset = mouseX-bx;
-  yOffset = mouseY-by;
-}
 
 
 function drawGrid(size){
