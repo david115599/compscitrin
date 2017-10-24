473afcd0f4cce36cf635be24c1ef205f65754b6f
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-10-24 10:44:35 -0400

    S
    S

--- My website compsci/Unit 2/Wasteland1/wasteland1.js
+++ My website compsci/Unit 2/Wasteland1/wasteland1.js
@@ -12,11 +12,21 @@ var overBox2 = false;
 var locked2 = false;
 var bx;
 var by;
+var theta = 0;
+var x1, y1, centerX, centerY;
+var speed;
 function setup(){
 createCanvas (500,500);
  slider = createSlider(0, width, 100);
  slider.position(10, 10);
  slider.style('width', '80px');
+ centerX=width/2;
+ centerY=height/2;
+ smooth();
+ font=loadFont("ArialMT-48.vlw");
+ textAlign(LEFT);
+ textFont(font, 12);
+ speed= .02;
 }
 function draw(){
 background(150,150,150);
@@ -27,6 +37,11 @@ ellipse (x-a, y-b, r, r);
 val = slider.value();
 r = val;
 translate(-width/2, -height/2);
+x1=cos(theta)*r/2 + centerX;
+y1=sin(theta)*r/2 + centerY
+ellipse(x1, y1, 10, 10);
+theta=theta+speed;
+
 }
 
 
