11ade140196f889a09068222a7194884604d3711
nsmadoff <nsmadoff@gmail.com>
2017-10-30 12:05:22 -0400

    d
    d

--- My website compsci/Unit 2/drawgrid/grid.js
+++ My website compsci/Unit 2/drawgrid/grid.js
@@ -4,32 +4,39 @@ var y = (200);
 var a = (0);
 var b = (0);
 var slider;
-var boxSize = 75;
-var overBox = false;
-var locked = false;
-var overBox2 = false;
-var locked2 = false;
+var val;
 var bx;
 var by;
-var radius;
+var a1=0; //angle
+var x1;//triangle coordinate x
+var y1;// triangle coordinate y
+var mx;
+var my;
 function setup(){
 createCanvas (500,500);
- slider = createSlider(0, 255, 100);
+
+ slider = createSlider(0, 500, 500);
  slider.position(10, 10);
  slider.style('width', '80px');
- background(150,150,150);
- drawGrid(25);
 }
-
 function draw(){
+background(150,150,150);
+drawGrid(25);
+fill(0,255,0);
+ellipse (mx, my, r, r);
+val = slider.value();
+r = val;
+if (a1<=360) {
+
+a1+=.01;
 
 }
+else {
+a1=0;
+}
 
-function mouseClicked() {
-  radius = slider.value();
-  ellipse (mouseX, mouseY, radius, radius);
-  return false;
-}  
+drawRight();
+}
 
 function drawGrid(size){
 for (var i = 0; i < width; i += 10){
@@ -48,4 +55,39 @@ for (var i = 0; i < width; i += 10){
   line(0,height/2,width,height/2);
   strokeWeight(1);
 
+}
+
+function drawRight() {
+
+
+
+translate(mx,my);
+x1 = r*cos(a1)/2 ;
+y1 = r*sin(a1)/2 ;
+
+fill(255,0,0);
+polygon(0,0,r/2,5);
+fill(0,0,255);
+triangle(0, 0, x1, y1,x1,0);
+triangle(0,0,-x1,-y1,-x1,0);
+
+}
+function mouseReleased() {
+  if(mouseReleased) {
+    mx=mouseX;
+    my=mouseY;
+    mx-=mx%10;
+    my-=my%10;
+}
+}
+
+function polygon(x, y, radius, npoints) {
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
