f93849ce3ab6a7ef610b3dfbd1383874c961f74f
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-10-31 17:49:03 -0400

    ,
    ,

--- My website compsci/Unit 2/LAB Circle 2/LAB.js
+++ My website compsci/Unit 2/LAB Circle 2/LAB.js
@@ -12,19 +12,17 @@ function setup() {
    }
 
 drawGrid2(25);
-translate(385,-5);
 rotate(90);
-drawGrid2(25);
 }
 
 function drawGrid2(size){
 
-for (var i = -600; i < 5*width; i += 50){
+for (var i = 00; i < width; i += 50){
 /*strokeWeight(5);
   line(0,i,width,i);
   line(i,0,i,height);*/
 strokeWeight(1);
-  line(i,W+40,-i+(W*2)-1,-i);
+  line(i,400,(400),350-i);
 
   }
 
