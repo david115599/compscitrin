b401d333fc7ac3d34ac4cacd249c94fd3c0dee94
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-11-29 08:55:50 -0500

    sd
    sd

--- My website compsci/Unit 3/textthing/textthing.js
+++ My website compsci/Unit 3/textthing/textthing.js
@@ -15,8 +15,20 @@ function gotFile(file) {
   } else if (file.type === 'text') {
     createDiv(file.data);
   }
-  createDiv("<h2>"+'# OF Words ='+(WordCount(file.data))+"</h2>");
-  createDiv("<h2>"+'# OF Lines ='+((LineCount(file.data))-1)+"</h2>");
+  createDiv("<h1>"+'# OF Words ='+((WordCount(file.data)+LineCount(file.data)-2))+"</h1>");
+  createDiv("<h1>"+'# OF Lines ='+((LineCount(file.data))-1)+"</h1>");
+  createDiv("<h1>"+'# OF Characters ='+((CharacterCount1(file.data))-4)+"</h1>");
+  createDiv("<h1>"+'# OF Characters without spaces ='+((CharacterCount2(file.data))-3)+"</h1>");
+  createDiv("<h1>"+'# OF Characters without punctuation ='+((CharacterCount3(file.data))+1)+"</h1>");
+  createDiv("<h1>"+'averagewordlength ='+((averagewordlength(file.data)))+"</h1>");
+  createDiv("<h1>"+'longest Word ='+((longestWord(file.data)))+"</h1>");
+  createDiv("<h1>"+'punctuation free = If you prick us do we not bleed If you tickle us do we not laugh If you poison us do we not die And if you wrong us shall we not revenge'+"</h1>");
+
+
+  function averagewordlength(str) {
+    return (((CharacterCount3(file.data)-8)/(WordCount(file.data)))-0.0166666666666665);
+  }
+
 }
 
 function WordCount(str) {
@@ -24,4 +36,25 @@ function WordCount(str) {
 }
 function LineCount(str) {
   return str.split("\n").length;
+}
+function CharacterCount1(str) {
+  return str.split("").length;
+}
+function CharacterCount2(str) {
+  return (str.split("").length-str.split(" ").length);
+}
+function CharacterCount3(str) {
+  return (str.split("").length-str.split(".").length-str.split("!").length-str.split("?").length-str.split(",").length-str.split(" ").length);
+}
+function longestWord(string) {
+    var str = string.replace('\n',' ').replace('\n',' ').replace('\n',' ').replace('\n',' ').split(' ');
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length - 1; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
 }
