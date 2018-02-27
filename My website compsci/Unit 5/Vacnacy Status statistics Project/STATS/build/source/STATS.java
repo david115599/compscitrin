5b7be03a06b401311e4c24dd4675e0c6ed92b367
david115599 <davidbershadsky@RaspberryPi-2.local>
2018-02-27 12:00:58 -0500

    z

--- My website compsci/Unit 5/Vacnacy Status statistics Project/STATS/build/source/STATS.java
+++ My website compsci/Unit 5/Vacnacy Status statistics Project/STATS/build/source/STATS.java
@@ -67,6 +67,9 @@ public void setup() {
   output.println("<title>" + states[i] + "</title>");
   output.println("</head><body>");
   output.println("<h1>" + states[i] + "</h1>");
+
+
+
   output.println("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis tortor ullamcorper, ultricies nunc at, varius libero. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla rutrum accumsan lectus vel bibendum. Ut sed hendrerit eros, et finibus ipsum. Maecenas eros magna, placerat at mi vel, sagittis lobortis sapien. Donec at dignissim elit, in egestas enim. Suspendisse condimentum, magna non porta hendrerit, lorem mauris tempor justo, sed imperdiet libero metus et nisi. Etiam eu orci et sem vulputate vestibulum dapibus in ipsum. Ut mi mauris, mollis quis condimentum eget, commodo nec felis. In varius augue viverra erat consequat convallis.</p>");
   output.println("</body></html>");
   output.flush(); // Writes the remaining data to the file
