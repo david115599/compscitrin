9cc55d55ffa0910bce0d7c28240898b805cda0c0
david115599 <davidbershadsky@RaspberryPi-2.home>
2018-03-07 14:26:30 -0500

    h

--- My website compsci/Unit 5/Vacnacy Status statistics Project/STATS/build/source/STATS.java
+++ My website compsci/Unit 5/Vacnacy Status statistics Project/STATS/build/source/STATS.java
@@ -85,58 +85,57 @@ int sf =5;
 public void setup() {
   
   usa = loadShape("usa-wikipedia.svg");
-   alabama = usa.getChild("AL");
-   alaska = usa.getChild("AK");
-   arizona = usa.getChild("AZ");
-   arkansas = usa.getChild("AR");
-   california = usa.getChild("CA");
-   colorado = usa.getChild("CO");
-   connecticcut = usa.getChild("CT");
-   delaware = usa.getChild("DE");
-   districtofcolumbia = usa.getChild("DC");
-   florida = usa.getChild("FL");
-   georgia = usa.getChild("GA");
-   hawaii = usa.getChild("HI");
-   idaho = usa.getChild("ID");
-   illinois = usa.getChild("IL");
-   indiana = usa.getChild("IN");
-   iowa = usa.getChild("IA");
-   kansas = usa.getChild("KS");
-   kentucky = usa.getChild("KY");
-   louisiana = usa.getChild("LA");
-   maine = usa.getChild("ME");
-   maryland = usa.getChild("MD");
-   massachusetts = usa.getChild("MA");
+  alabama = usa.getChild("AL");
+  alaska = usa.getChild("AK");
+  arizona = usa.getChild("AZ");
+  arkansas = usa.getChild("AR");
+  california = usa.getChild("CA");
+  colorado = usa.getChild("CO");
+  connecticcut = usa.getChild("CT");
+  delaware = usa.getChild("DE");
+  districtofcolumbia = usa.getChild("DC");
+  florida = usa.getChild("FL");
+  georgia = usa.getChild("GA");
+  hawaii = usa.getChild("HI");
+  idaho = usa.getChild("ID");
+  illinois = usa.getChild("IL");
+  indiana = usa.getChild("IN");
+  iowa = usa.getChild("IA");
+  kansas = usa.getChild("KS");
+  kentucky = usa.getChild("KY");
+  louisiana = usa.getChild("LA");
+  maine = usa.getChild("ME");
+  maryland = usa.getChild("MD");
+  massachusetts = usa.getChild("MA");
   michigan = usa.getChild("MI");
-   minnesota = usa.getChild("MN");
-   mississippi = usa.getChild("MS");
-   missouri = usa.getChild("MO");
-   montana = usa.getChild("MT");
-   nebraska = usa.getChild("NE");
-   nevada = usa.getChild("NV");
-   newhampshire = usa.getChild("NH");
-   newjersey = usa.getChild("NJ");
-   newmexico = usa.getChild("NM");
-   newyork = usa.getChild("NY");
-   northcarolina = usa.getChild("NC");
-   northdakota = usa.getChild("ND");
+  minnesota = usa.getChild("MN");
+  mississippi = usa.getChild("MS");
+  missouri = usa.getChild("MO");
+  montana = usa.getChild("MT");
+  nebraska = usa.getChild("NE");
+  nevada = usa.getChild("NV");
+  newhampshire = usa.getChild("NH");
+  newjersey = usa.getChild("NJ");
+  newmexico = usa.getChild("NM");
+  newyork = usa.getChild("NY");
+  northcarolina = usa.getChild("NC");
+  northdakota = usa.getChild("ND");
   ohio = usa.getChild("OH");
-   oklahoma = usa.getChild("OK");
-   oregon = usa.getChild("OR");
-   pennsylvania = usa.getChild("PA");
-   rhodeisland = usa.getChild("RI");
-   southcarolina = usa.getChild("SC");
-   southdakota = usa.getChild("SD");
-   tennessee = usa.getChild("TN");
-   texas = usa.getChild("TX");
-   utah = usa.getChild("UT");
-   vermont = usa.getChild("VT");
-   virginia = usa.getChild("VA");
-   washington = usa.getChild("WA");
-   westvirginia = usa.getChild("WV");
-   wisconsin = usa.getChild("WI");
-   wyoming = usa.getChild("WY");
-
+  oklahoma = usa.getChild("OK");
+  oregon = usa.getChild("OR");
+  pennsylvania = usa.getChild("PA");
+  rhodeisland = usa.getChild("RI");
+  southcarolina = usa.getChild("SC");
+  southdakota = usa.getChild("SD");
+  tennessee = usa.getChild("TN");
+  texas = usa.getChild("TX");
+  utah = usa.getChild("UT");
+  vermont = usa.getChild("VT");
+  virginia = usa.getChild("VA");
+  washington = usa.getChild("WA");
+  westvirginia = usa.getChild("WV");
+  wisconsin = usa.getChild("WI");
+  wyoming = usa.getChild("WY");
   JSONArray json;
   JSONArray json1;
   states = loadStrings("State_Names.txt");
@@ -149,8 +148,8 @@ public void setup() {
   output.println("<h1>Migrant Vacancy Status Statistics in US Between 2000 and 2010</h1>");
   for (int i = 1; i < json.size(); i++) {
     String state = json.getJSONArray(i).getString(1); //states[i-1]
-  output.println("<a href='website/"+state+".html'>"+state+"</a>");
-}
+    output.println("<a href='website/"+state+".html'>"+state+"</a>");
+  }
   output.println("<svg width=\"2000\" height=\"15000\" xmlns=\"http://www.w3.org/2000/svg\">");
   //load JSON into arrays (state,pop2010)
   for (int i = 1; i < json.size(); i++) {
@@ -171,47 +170,46 @@ public void setup() {
   output.flush(); // Writes the remaining data to the file
   output.close(); // Finishes the file
   println("Done");
-  for (int i=0;i<vacancy2000.length ;i++ ) {
-  national2000+=vacancy2000[i];
+  for (int i=0; i<vacancy2000.length; i++ ) {
+    national2000+=vacancy2000[i];
   }
-  for (int i=0;i<vacancy2010.length ;i++ ) {
-  national2010+=vacancy2010[i];
+  for (int i=0; i<vacancy2010.length; i++ ) {
+    national2010+=vacancy2010[i];
   }
-  for(int i = 0; i<states.length; i++){
-  output = createWriter("website/"+states[i]+".html");
-  output.println("<html><head>");
-  output.println("<title>" + states[i] + "</title>");
-  output.println("</head><body>");
-  output.println("<h1>" + states[i] + "</h1>");
-  output.println("<h1> Number of vacant housing units for migrants in "+ states[i] +" in 2000: " + vacancy2000[i] + " units</h1>");
-  output.println("<h1> Total Number of vacant housing units for migrants in 2000: " + national2000 + " units</h1>");
-  output.println("<h1> Number of vacant housing units for migrants in 2010: " + vacancy2010[i] + " units</h1>");
-  output.println("<h1> Total Number of vacant housing units for migrants in "+ states[i] +" in 2000: " + national2010 + " units</h1>");
-  output.println("<svg width=\"2000\" height=\"20000\" xmlns=\"http://www.w3.org/2000/svg\">");
-  output.println("<rect y='0' x='40' height='"+ vacancy2000[i]/(sf*10)+"' width='100' fill='darkred'/>");
-  output.println("<rect y='0' x='140' height='"+ national2000/(sf*10)+"' width='100' fill='yellow'/>");
-  output.println("<rect y='0' x='240' height='"+ (vacancy2010[i]/(sf*10))+"' width='100' fill='darkred'/>");
-  output.println("<rect y='0' x='340' height='"+ (national2010/(sf*10))+"' width='100' fill='yellow'/>");
-  output.println("<text y='"+(vacancy2000[i]/(sf*10)+40)+"' x='40' fill='green' font-size='40'>"+vacancy2000[i]+"  </text>");
-  output.println("<text y='"+(vacancy2010[i]/(sf*10)+40)+"' x='240' fill='green' font-size='40'>"+vacancy2010[i]+"  </text>");
-  output.println("<text y='"+(national2000/(sf*10)+40)+"' x='140' fill='green' font-size='40'>"+national2000+"  </text>");
-  output.println("<text y='"+(national2010/(sf*10)+40)+"' x='340' fill='green' font-size='40'>"+national2010+"  </text>");
-  output.println("</body></html>");
-  output.flush(); // Writes the remaining data to the file
-  output.close(); // Finishes the file
-  println("Done");
+  for (int i = 0; i<states.length; i++) {
+    output = createWriter("website/"+states[i]+".html");
+    output.println("<html><head>");
+    output.println("<title>" + states[i] + "</title>");
+    output.println("</head><body>");
+    output.println("<h1>" + states[i] + "</h1>");
+    output.println("<h1> Number of vacant housing units for migrants in "+ states[i] +" in 2000: " + vacancy2000[i] + " units</h1>");
+    output.println("<h1> Total Number of vacant housing units for migrants in 2000: " + national2000 + " units</h1>");
+    output.println("<h1> Number of vacant housing units for migrants in 2010: " + vacancy2010[i] + " units</h1>");
+    output.println("<h1> Total Number of vacant housing units for migrants in "+ states[i] +" in 2000: " + national2010 + " units</h1>");
+    output.println("<svg width=\"2000\" height=\"20000\" xmlns=\"http://www.w3.org/2000/svg\">");
+    output.println("<rect y='0' x='40' height='"+ vacancy2000[i]/(sf*10)+"' width='100' fill='darkred'/>");
+    output.println("<rect y='0' x='140' height='"+ national2000/(sf*10)+"' width='100' fill='yellow'/>");
+    output.println("<rect y='0' x='240' height='"+ (vacancy2010[i]/(sf*10))+"' width='100' fill='darkred'/>");
+    output.println("<rect y='0' x='340' height='"+ (national2010/(sf*10))+"' width='100' fill='yellow'/>");
+    output.println("<text y='"+(vacancy2000[i]/(sf*10)+40)+"' x='40' fill='green' font-size='40'>"+vacancy2000[i]+"  </text>");
+    output.println("<text y='"+(vacancy2010[i]/(sf*10)+40)+"' x='240' fill='green' font-size='40'>"+vacancy2010[i]+"  </text>");
+    output.println("<text y='"+(national2000/(sf*10)+40)+"' x='140' fill='green' font-size='40'>"+national2000+"  </text>");
+    output.println("<text y='"+(national2010/(sf*10)+40)+"' x='340' fill='green' font-size='40'>"+national2010+"  </text>");
+    output.println("</body></html>");
+    output.flush(); // Writes the remaining data to the file
+    output.close(); // Finishes the file
+    println("Done");
   }
 }
 public void draw() {
   background(255);
-
   // Draw the full map
   shape(usa, -250, 0);
 
   // Disable the colors found in the SVG file
   michigan.disableStyle();
   // Set our own coloring
-  fill(0, 51, 102);
+  fill(0, 51, 127.5f+(vacancy2000[23]-vacancy2010[23])/10);
   noStroke();
   // Draw a single state
   shape(michigan, -250, 0); // Wolverines!
