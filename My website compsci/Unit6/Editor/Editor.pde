a8e763ba15a594ed5ad00c5a1a3c36a7cf8158f2
david115599 <davidbershadsky@RaspberryPi-2.local>
2018-03-12 09:46:24 -0400

    x

--- My website compsci/Unit6/Editor/Editor.pde
+++ My website compsci/Unit6/Editor/Editor.pde
@@ -7,6 +7,7 @@
  */
 
 //import controlP5 and create an object to add buttons
+/*
 import controlP5.*;
 
 ControlP5 cp5;
@@ -57,5 +58,49 @@ void floor2(int theValue) {
     println("erasing");
     fill(0);
     break;
+  }
+}
+*/
+
+// Constants
+int Y_AXIS = 1;
+int X_AXIS = 2;
+color c1, c2;
+
+void setup() {
+  size(620, 640);
+  background(255);
+
+  // Define colors
+  c1 = color(255);
+  c2 = color(0);
+
+  noLoop();
+}
+
+void draw() {
+  setGradient(50, 190, 540, 80, c2, c1, X_AXIS);
+}
+
+void setGradient(int x, int y, float w, float h, color c1, color c2, int axis ) {
+
+  noFill();
+
+  if (axis == Y_AXIS) {  // Top to bottom gradient
+    for (int i = y; i <= y+h; i+=33.75) {
+      float inter = map(i, y, y+h, 0, 1);
+      color c = lerpColor(c1, c2, inter);
+      fill(c);
+      rect(x, i, x+w, i);
+    }
+  }
+  else if (axis == X_AXIS) {  // Left to right gradient
+    for (int i = x; i <= x+w; i+=33.75) {
+      float inter = map(i, x, x+w, 0, 1);
+      color c = lerpColor(c1, c2, inter);
+      fill(c);
+      noStroke();
+      rect(i, y, i, y+h);
+    }
   }
 }
--- My website compsci/Unit6/Editor/build/source/Editor.java
+++ My website compsci/Unit6/Editor/build/source/Editor.java
@@ -3,8 +3,6 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
-import controlP5.*; 
-
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -25,15 +23,16 @@ public class Editor extends PApplet {
  */
 
 //import controlP5 and create an object to add buttons
-
+/*
+import controlP5.*;
 
 ControlP5 cp5;
 
 int state = 1; //start on floor 1
 
-public void setup() {
+void setup() {
   rectMode(CENTER);
-  
+  size(400, 600);
   background(100);
 
   cp5 = new ControlP5(this);
@@ -49,7 +48,7 @@ public void setup() {
     .setValue(1);
 }
 
-public void draw() {
+void draw() {
   if (state == 1) {
     println("Drawing");
     fill(250);
@@ -61,7 +60,7 @@ public void draw() {
 
 }
 
-public void floor1(int theValue) {
+void floor1(int theValue) {
   if (state == 1) {
     println("Drawing");
     fill(250);
@@ -69,7 +68,7 @@ public void floor1(int theValue) {
 }
 
 
-public void floor2(int theValue) {
+void floor2(int theValue) {
   switch (state) {
   case 2:
     println("erasing");
@@ -77,7 +76,51 @@ public void floor2(int theValue) {
     break;
   }
 }
-  public void settings() {  size(400, 600); }
+*/
+
+// Constants
+int Y_AXIS = 1;
+int X_AXIS = 2;
+int c1, c2;
+
+public void setup() {
+  
+  background(255);
+
+  // Define colors
+  c1 = color(255);
+  c2 = color(0);
+
+  noLoop();
+}
+
+public void draw() {
+  setGradient(50, 190, 540, 80, c2, c1, X_AXIS);
+}
+
+public void setGradient(int x, int y, float w, float h, int c1, int c2, int axis ) {
+
+  noFill();
+
+  if (axis == Y_AXIS) {  // Top to bottom gradient
+    for (int i = y; i <= y+h; i+=33.75f) {
+      float inter = map(i, y, y+h, 0, 1);
+      int c = lerpColor(c1, c2, inter);
+      fill(c);
+      rect(x, i, x+w, i);
+    }
+  }
+  else if (axis == X_AXIS) {  // Left to right gradient
+    for (int i = x; i <= x+w; i+=33.75f) {
+      float inter = map(i, x, x+w, 0, 1);
+      int c = lerpColor(c1, c2, inter);
+      fill(c);
+      noStroke();
+      rect(i, y, i, y+h);
+    }
+  }
+}
+  public void settings() {  size(620, 640); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Editor" };
     if (passedArgs != null) {
