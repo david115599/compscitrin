d74192661ae5637938271b42312670563e7418d2
david115599 <davidbershadsky@RaspberryPi-3.local>
2018-04-12 12:34:51 -0400

    a

--- My website compsci/Unit6/Editor/build/source/Editor.java
+++ My website compsci/Unit6/Editor/build/source/Editor.java
@@ -107,68 +107,86 @@ public void draw() {
   rect(275, 120, 20, 20);
 
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >5 & mouseX <65 & mouseY >205 & mouseY <235) {
+    tool =1;
+    fill(255, 0, 0);
+  }
   rect(5, 205, 60, 30);
   fill(150, 150, 150);
   text("PEN", 5, 230);
-  if (mousePressed == true & mouseX >5 & mouseX <65 & mouseY >205 & mouseY <235) {
-    tool =1;
-  }
+
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >70 & mouseX <130 & mouseY >205 & mouseY <235) {
+    tool =2;
+      fill(255, 0, 0);
+  }
   rect(70, 205, 60, 30);
   fill(150, 150, 150);
   text("ERS", 70, 230);
-  if (mousePressed == true & mouseX >70 & mouseX <130 & mouseY >205 & mouseY <235) {
-    tool =2;
-  }
+
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >135 & mouseX <195 & mouseY >205 & mouseY <235) {
+    thickness ++;
+    fill(255, 0, 0);
+  }
   rect(135, 205, 60, 30);
   fill(150, 150, 150);
   text("S+", 135, 230);
-  if (mousePressed == true & mouseX >135 & mouseX <195 & mouseY >205 & mouseY <235) {
-    thickness ++;
-  }
+
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >200 & mouseX <260 & mouseY >205 & mouseY <235 & thickness>1) {
+    thickness --;
+    fill(255, 0, 0);
+  }
   rect(200, 205, 60, 30);
   fill(150, 150, 150);
   text("S-", 200, 230);
-  if (mousePressed == true & mouseX >200 & mouseX <260 & mouseY >205 & mouseY <235 & thickness>1) {
-    thickness --;
-  }
+
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >135 & mouseX <195 & mouseY >240 & mouseY <270) {
+    A1 ++;
+    fill(255, 0, 0);
+  }
   rect(135, 240, 60, 30);
   fill(150, 150, 150);
   text("A1+", 135, 265);
-  if (mousePressed == true & mouseX >135 & mouseX <195 & mouseY >240 & mouseY <270) {
-    A1 ++;
-  }
+
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >200 & mouseX <260 & mouseY >240 & mouseY <270 & thickness>1) {
+    A1 --;
+    fill(255, 0, 0);
+  }
   rect(200, 240, 60, 30);
   fill(150, 150, 150);
   text("A1-", 200, 265);
-  if (mousePressed == true & mouseX >200 & mouseX <260 & mouseY >240 & mouseY <270 & thickness>1) {
-    A1 --;
-  }
+
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >135 & mouseX <195 & mouseY >275 & mouseY <305) {
+    A2 ++;
+    fill(255, 0, 0);
+  }
   rect(135, 275, 60, 30);
   fill(150, 150, 150);
   text("A2+", 135, 275+25);
-  if (mousePressed == true & mouseX >135 & mouseX <195 & mouseY >275 & mouseY <305) {
-    A2 ++;
-  }
+
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >200 & mouseX <260 & mouseY >275 & mouseY <305 & thickness>1) {
+    A2 --;
+    fill(255, 0, 0);
+  }
   rect(200, 275, 60, 30);
   fill(150, 150, 150);
   text("A2-", 200, 275+25);
-  if (mousePressed == true & mouseX >200 & mouseX <260 & mouseY >275 & mouseY <305 & thickness>1) {
-    A2 --;
-  }
+
   fill(30, 30, 30);
+  if (mousePressed == true & mouseX >5 & mouseX <65 & mouseY >240 & mouseY <270) {
+    tool =3;
+    fill(255, 0, 0);
+  }
   rect(5, 240, 60, 30);
   fill(150, 150, 150);
   text("LIN", 5, 265);
-  if (mousePressed == true & mouseX >5 & mouseX <65 & mouseY >240 & mouseY <270) {
-    tool =3;
-  }
+
   fill(30, 30, 30);
   if (mousePressed == true & mouseX >5 & mouseX <75 & mouseY >275 & mouseY <305) {
       PImage partialSave = get(300,0,900,600);
