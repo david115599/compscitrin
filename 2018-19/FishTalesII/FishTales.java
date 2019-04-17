b2efea12a440842d83687221c3e341edc5f1f969
Alex Sheen <alexander.sheen19@trinityschoolnyc.org>
2019-04-17 10:40:31 -0400

    jgih

--- 2018-19/FishTalesII/FishTales.java
+++ 2018-19/FishTalesII/FishTales.java
@@ -17,41 +17,26 @@ public class FishTales{
 
     // main animation loop
     while (true)  {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 3c2821686ce141d8b7ca91e89667c4b1d6f96323
 
         if(StdDraw.isKeyPressed(71)){ //zoom in 'i' 'I'
 
           myTank.add(new Goldfish("Goldfish",0,0,0.05f,0.05f));
         }
 
+        else if(StdDraw.isKeyPressed(80)){ //zoom in 'i' 'I'
+
+          myTank.add(new Piranha("Piranha",0,0,0.05f,0.05f));
+        }
+
         StdDraw.clear();
         myTank.update();
-      
-
 
 
       StdDraw.show();// copy offscreen buffer to onscreen
 
       StdDraw.pause(20);// pause for 20 ms
     }//main animation loop
-<<<<<<< HEAD
-=======
->>>>>>> 741fc9d05d6e2f8613720af0b6c278a43f0399f7
-=======
-      StdDraw.clear();
-      g.update();
-      p.update();
 
-      StdDraw.show();// copy offscreen buffer to onscreen
-
-      StdDraw.pause(20);// pause for 20 ms
-    }//main animation loop
->>>>>>> 741fc9d05d6e2f8613720af0b6c278a43f0399f7
->>>>>>> 3c2821686ce141d8b7ca91e89667c4b1d6f96323
   }//main
 
 }//FishTales class
