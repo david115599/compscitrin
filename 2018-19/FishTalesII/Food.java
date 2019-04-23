e41d1755f6e3aa4aace473c552285e7a8af44858
Oliver D'Avolio <oliver.d'avolio21@trinityschoolnyc.org>
2019-04-23 10:27:53 -0400

    fooodss

--- 2018-19/FishTalesII/Food.java
+++ 2018-19/FishTalesII/Food.java
@@ -4,12 +4,14 @@ public class Food extends Pellet{
 
   Food(){
     super(new Color(0,255,0), new Color(255,255,255));
-    this.yVelocity = Math.random()*0.015;
+    this.yVelocity = -Math.random()*0.005;
+    this.xPos = 1 - Math.random()*2;
   }
 
   protected void show(){
-    StdDraw.setPenColor(StdDraw.YELLOW);
+    StdDraw.setPenColor(StdDraw.BLACK);
     StdDraw.filledCircle(this.xPos, this.yPos, 0.02);
+
   }
 
   public boolean isDead(){
