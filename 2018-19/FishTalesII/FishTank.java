e325904b67fb3bf1be1fd78d3174e71c21d88ef8
david115599 <david.bershadsky2@gmail.com>
2019-05-10 11:07:43 -0400

    asd

--- 2018-19/FishTalesII/FishTank.java
+++ 2018-19/FishTalesII/FishTank.java
@@ -99,10 +99,9 @@ public class FishTank{
             if(myStuff.get(z)!= myStuff.get(i) && Math.sqrt(Math.pow((Math.abs(myStuff.get(i).getX()-myStuff.get(z).getX())),2)+Math.pow(Math.abs(myStuff.get(i).getY()-myStuff.get(z).getY()),2)) <= ((myStuff.get(i).getSize())+myStuff.get(z).getSize())*2){
           // do we want this in the if statment (myStuff.get(z).isDead() == false && myStuff.get(i).isDead() == false)
         //  if(myStuff.get(i).hasCollision(myStuff.get(z)) == true){
-
-            if (myStuff.get(i) instanceof Fish && myStuff.get(z) instanceof Fish && myStuff.get(z).isDead() == false && myStuff.get(i).isDead() == false || myStuff.get(i) instanceof Fish && myStuff.get(z) instanceof Pellet && myStuff.get(z).isDead() == false && myStuff.get(i).isDead() == false ) {
+          myStuff.get(i).hasCollision(myStuff.get(z));
+            if (myStuff.get(z)!= myStuff.get(i) && myStuff.get(i) instanceof Fish && myStuff.get(z) instanceof Fish && myStuff.get(z).isDead() == false && myStuff.get(i).isDead() == false || myStuff.get(i) instanceof Fish && myStuff.get(z) instanceof Pellet && myStuff.get(z).isDead() == false && myStuff.get(i).isDead() == false ) {
               add(new Bubble(myStuff.get(i).getX()-.1,myStuff.get(i).getY()-.1));
-              add(new Bubble(myStuff.get(z).getX()-.1,myStuff.get(z).getY()-.1));
             }
 
             if (myStuff.get(i) instanceof Goldfish && myStuff.get(z) instanceof Goldfish) {
