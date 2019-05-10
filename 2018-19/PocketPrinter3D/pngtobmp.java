97499e513796457cfdd362b62a784960b44cf015
david115599 <david.bershadsky2@gmail.com>
2019-05-10 12:43:01 -0400

    asd

--- 2018-19/PocketPrinter3D/pngtobmp.java
+++ 2018-19/PocketPrinter3D/pngtobmp.java
@@ -1,24 +1,46 @@
-import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
-import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class pngtobmp {
 
-  public static void main(String[] args) {
+  public static void main( final String[] args ) {
     try {
+    File file = new File("test.png");
+    BufferedImage bi = ImageIO.read(file);
+    int scalarx = bi.getWidth()/128;
+    int scalary = bi.getHeight()/160;
+    BufferedImage img = map( 128, 160 , bi, scalarx ,scalary );
+    savePNG( img, "test.bmp" );
+  }catch (IOException e) {
+    System.out.println("Exception occured :" + e.getMessage());
+  }
+  }
 
-      File file = new File("./test.png");
-      BufferedImage bi = ImageIO.read(file);
+  private static BufferedImage map( int sizeX, int sizeY, BufferedImage bi, int scalarx , int scalary){
+    final BufferedImage res = new BufferedImage( sizeX, sizeY, BufferedImage.TYPE_INT_RGB );
+    int w = bi.getWidth();
+     int h = bi.getHeight();
 
-      ImageIO.write(bi, "png", new File("./image.png"));
-      ImageIO.write(bi, "jpeg", new File("./image.jpeg"));
-      ImageIO.write(bi, "BMP", new File("./image.bmp"));
+    for (int x = 0; x < sizeX; x++){
+      for (int y = 0; y < sizeY; y++){
+        res.setRGB(x, y, bi.getRGB( x*scalarx, y*scalary ) );
+      }
+    }
+    return res;
+  }
 
-      System.out.println("Images were written succesfully.");
-
-    } catch (IOException e) {
-      System.out.println("Exception occured :" + e.getMessage());
+  private static void savePNG( final BufferedImage bi, final String path ){
+    try {
+      RenderedImage rendImage = bi;
+      ImageIO.write(rendImage, "bmp", new File(path));
+      ImageIO.write(rendImage, "PNG", new File(path));
+      ImageIO.write(rendImage, "jpeg", new File(path));
+    } catch ( IOException e) {
+      e.printStackTrace();
     }
   }
 
