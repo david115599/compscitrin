fe47985805020060c938ac01f5615f63d25cc3f0
david115599 <david.bershadsky2@gmail.com>
2019-05-31 11:55:35 -0400

    adas
    asdsadasd

--- 2018-19/PocketPrinter3D/app/PocketPrinter3D/PocketPrinter3D/AppDelegate.swift
+++ 2018-19/PocketPrinter3D/app/PocketPrinter3D/PocketPrinter3D/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  PocketPrinter3D
 //
 //  Created by David Bershadsky on 5/31/19.
-//  Copyright © 2019 David Bershadsky. All rights reserved.
+//  Copyright © 2019 David Bershadsky And Leah Teichholtz. All rights reserved.
 //
 
 import UIKit
