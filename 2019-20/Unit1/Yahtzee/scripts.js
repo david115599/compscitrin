1892de94fdc7655e474c57e454538c93825ee258
david115599 <david.bershadsky2@gmail.com>
2019-09-16 09:00:51 -0400

    cgvbn

---
+++ 2019-20/Unit1/Yahtzee/scripts.js
@@ -0,0 +1,4 @@
+console.log("Scripts.js loaded");
+var roll_button = document.getElementById("roll_button")
+roll_button.addEventListener('click', function(){
+  console.log("roll button clicked")});
