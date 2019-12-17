26384fcd1ceb2536a8359d06e1d45777ce9d2308
david115599 <david.bershadsky2@gmail.com>
2019-12-16 19:44:38 -0500

    ds
    dsf

--- 2019-20/Unit2/Walking_Skeleton_Project/walkingskeleton.js
+++ 2019-20/Unit2/Walking_Skeleton_Project/walkingskeleton.js
@@ -2,56 +2,73 @@ var express = require('express');
 var fs = require('fs');
 var favicon = require('serve-favicon');
 
-
-var app = express();
+var app = express(); //Create an Express route
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(favicon(__dirname + '/public/images/logo.png'));
 
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log('Server started at '+ new Date()+', on port ' + port+'!');
+var port = 3000;
+app.listen(port, function() {
+  console.log('Server started at ' + new Date() + ', on port ' + port + '!');
 });
 
-app.get('/', function(request, response){
+app.get('/', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render('index');
+  response.render('login');
 });
-
-app.get('/login', function(request, response){
-  var user_data={
-      name: request.query.player_name
-  };
+app.get('/login', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render('game', {user:user_data});
+  response.render('login');
 });
-
-app.get('/:user/results', function(request, response){
-  var user_data={
-      name: request.params.user,
-      weapon: request.query.weapon
-  };
+app.get('/logout', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.send(JSON.stringify(user_data));
+  response.render('login');
 });
 
-app.get('/rules', function(request, response){
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render('rules');
-});
-
-app.get('/stats', function(request, response){
+app.get('/stats', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
   response.render('stats');
 });
-app.get('/about', function(request, response){
+
+app.get('/about', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
   response.render('about');
+});
+
+app.get('/game', function(request, response) {
+  var user_data = {
+    name: request.query.username,
+    password: request.query.password
+  };
+  console.log(user_data);
+  var logged_in = false;
+  //add logged in logic
+
+  if (logged_in) {
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render('game', {
+      user: user_data
+    });
+  } else {
+    response.status(403);
+    response.setHeader('Content-Type', 'text/html')
+    response.render('error');
+  }
+});
+
+app.post('/:user/game', function(request, response) {
+  var user_data = {
+    name: request.params.user,
+    weapon: request.query.weapon
+  };
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.send(JSON.stringify(user_data));
 });
