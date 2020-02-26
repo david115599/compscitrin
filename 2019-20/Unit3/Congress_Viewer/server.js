1def7afe9e50fcb7dd8f112577c88f1acd14c6c3
david115599 <david.bershadsky2@gmail.com>
2020-02-26 12:05:43 -0500

    asdsadasdasdasdasd

--- 2019-20/Unit3/Congress_Viewer/server.js
+++ 2019-20/Unit3/Congress_Viewer/server.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var favicon = require('serve-favicon');
 var app = express();
 var methodOverride = require('method-override');
+const apirequest = require('request');
+
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded());
@@ -23,10 +25,25 @@ app.get('/', function(request, response){
   response.render('index',{feedback:""});
 });
 
-app.get('/view', function(request, response){
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render('politicians',{feedback:""});
+app.get('/view', function(request, responsea){
+  responsea.status(200);
+  responsea.setHeader('Content-Type', 'text/html')
+
+  var options = {
+    url: "https://api.propublica.org/congress/v1/116/senate/members.json",
+    json: true,
+    headers: {
+      "X-API-Key": "wvz6nlmPtKUxSUTEDqeDaJpO1Wkv8jC6zpEQZups"
+    }
+  }
+
+  apirequest.get(options, function(error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body.results[0].members); // Print the HTML for the Google homepage.
+  responsea.render('politicians',{feedback:body});
+  });
+
 });
 
 app.get('/logout', function(request, response){
