8789fb627e1f1f4501102fb6e987a72fd487145a
david115599 <david.bershadsky2@gmail.com>
2020-03-11 08:24:19 -0400

    Revert "asdad"
    This reverts commit e8c0f13df20335cec6e29d8e19412c7fc495a9ce.

--- 2019-20/Unit3/Congress_Viewer/public/scripts/scripts.js
+++ 2019-20/Unit3/Congress_Viewer/public/scripts/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-  $(".modalbutton").click(function(event) {
-    var id = $(this).attr('id');
+  $("#my_button").click(function(event) {
+
     var apiKey = 'wvz6nlmPtKUxSUTEDqeDaJpO1Wkv8jC6zpEQZups';
-    var requestURL = 'https://api.propublica.org/congress/v1/members/'+id+'/bills/cosponsored.json';
+    var requestURL = 'https://api.propublica.org/congress/v1/bills/search.json?query=sanders';
 
     $.ajax({
       url: requestURL,
@@ -12,89 +12,14 @@ $(document).ready(function() {
         'X-API-Key': apiKey
       }
     }).done(function(data) {
-      var content =" ";
+      var content;
       for (var i = 0; i < data.results[0].bills.length; i++) {
         var bill = data.results[0].bills[i];
-        content +='<div class="card">'
-        content += '<div class="card-header" id="headingOne">';
-        content += '<h2 class="mb-0">';
-        content +='<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">';
-        content += "" + bill.short_title + "";
-        content +='</button>';
-        content +='</h2></div>  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample"> <div class="card-body">  <div id="bills">';
-        content += "" + bill.title + ""
-        content +='</div></div></div></div>';
+        content += "<h1>" + bill.short_title + "</h1>";
+        content += "<h3>" + bill.title + "</h3>"
       }
-      $('#'+id+'bills').html(content);
+      $('#bills').html(content);
       console.log(data)
     });
   });
 });
-
-
-
-/*
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-       <div class="modal-dialog" role="document">
-         <div class="modal-content">
-           <div class="modal-header">
-             <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-               <span aria-hidden="true">&times;</span>
-             </button>
-           </div>
-           <div class="modal-body">
-             <div class="accordion" id="accordionExample">
-               <div class="card">
-                 <div class="card-header" id="headingOne">
-                   <h2 class="mb-0">
-                     <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                       Collapsible Group Item #1
-                     </button>
-                   </h2>
-                 </div>
-
-                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                   <div class="card-body">
-                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                   </div>
-                 </div>
-               </div>
-               <div class="card">
-                 <div class="card-header" id="headingTwo">
-                   <h2 class="mb-0">
-                     <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                       Collapsible Group Item #2
-                     </button>
-                   </h2>
-                 </div>
-                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                   <div class="card-body">
-                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                   </div>
-                 </div>
-               </div>
-               <div class="card">
-                 <div class="card-header" id="headingThree">
-                   <h2 class="mb-0">
-                     <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                       Collapsible Group Item #3
-                     </button>
-                   </h2>
-                 </div>
-                 <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                   <div class="card-body">
-                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                   </div>
-                 </div>
-               </div>
-             </div>
-           </div>
-           <div class="modal-footer">
-             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-             <button type="button" class="btn btn-primary">Save changes</button>
-           </div>
-         </div>
-       </div>
-     </div>
-*/
