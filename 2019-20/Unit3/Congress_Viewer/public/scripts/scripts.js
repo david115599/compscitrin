28558b17be72c3edd161fd85cac9ddb43cef6b16
david115599 <david.bershadsky2@gmail.com>
2020-03-11 08:27:43 -0400

    sasd
    sasd

--- 2019-20/Unit3/Congress_Viewer/public/scripts/scripts.js
+++ 2019-20/Unit3/Congress_Viewer/public/scripts/scripts.js
@@ -15,11 +15,39 @@ $(document).ready(function() {
       var content;
       for (var i = 0; i < data.results[0].bills.length; i++) {
         var bill = data.results[0].bills[i];
-        content += "<h1>" + bill.short_title + "</h1>";
-        content += "<h3>" + bill.title + "</h3>"
+        content +='<div class="card">'
+        content += '<div class="card-header" id="heading'+i+'">';
+        content += '<h2 class="mb-0">';
+        content +='<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'">';
+        content += "" + bill.short_title + "";
+        content +='</button>';
+        content +='</h2></div>  <div id="collapse'+i+'" class="collapse" aria-labelledby="heading'+i+'" data-parent="#accordionExample"> <div class="card-body">  <div id="bills">';
+        content += "" + bill.title + ""
+        content +='</div></div></div></div>';
       }
       $('#bills').html(content);
       console.log(data)
     });
+  });
+});
+
+
+$(document).ready(function() {
+  $("#corrup").click(function(event) {
+    var val = $("#corrup").attr(value);
+    if (val == 0) {
+      var content =" ";
+      for (var i =116; i >=102;  i-- ) {
+        content+=  '<option value= '+i+' >'+i+' </option>'
+      }
+      $('#years').html(content);
+    }
+    else {
+      var content =" ";
+      for (var i =116; i >=89;  i-- ) {
+        content+=  '<option value= '+i+' >'+i+' </option>'
+      }
+      $('#years').html(content);
+    }
   });
 });
